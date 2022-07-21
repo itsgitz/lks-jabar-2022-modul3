cae6f419ac154523585cf39e68f9a3ab87261162
itsgitz <anggit.ginanjar@outlook.com>
2022-07-21 23:42:29 +0700

    fix(auth): Prevent duplicate user register

--- routes/auth.js
+++ routes/auth.js
@@ -21,7 +21,7 @@ router.post('/login', async function(req, res, next) {
   const username = req.body.username;
   const password = req.body.password;
 
-  const buff = new Buffer.from(req.body.password);
+  const buff = new Buffer.from(password);
   const encodedPassword = buff.toString('base64');
 
   let user = await database.getUserByUsername(username, encodedPassword)
@@ -41,22 +41,40 @@ router.post('/login', async function(req, res, next) {
 });
 
 router.get('/register', function(req, res, next) {
+  console.log('session message:', req.session.message);
+  let message;
+  if (!req.session.message) {
+    message = '';
+  } else {
+    message = req.session.message
+  }
+
   res.render('auth/register', {
     formTitle: 'Sign up',
     formUrl: req.originalUrl,
-    message: ''
+    message: message
   });
 });
 
 router.post('/register', async function (req, res, next) {
   let buff = new Buffer.from(req.body.password);
   let password = buff.toString('base64');
+
+  let user = await database.getUserByUsername(req.body.username, encodedPassword)
+  console.log(user)
+
+  if (user) {
+    req.session.message = 'Username already exist';
+    return res.redirect('/auth/register');
+  }
+
   let insertUser = await database.insertUser(req.body.username, password);
 
   console.log('insert user', insertUser)
 
   req.session.login = true;
   req.session.username = req.body.username;
+  req.session.message = '';
 
   return res.redirect('/');
 })
